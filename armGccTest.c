77750d42ae9d67dc313b19aa16af157828ffb93b
chris p <pavlidic@ece.auth.gr>
2020-11-22 03:18:09 +0200

    more extensive testing

--- armGccTest.c
+++ armGccTest.c
@@ -2,14 +2,15 @@
 
 int main(int argc, char const *argv[])
 {
-    int A[5],B[5],C[5];
-    for(int i=0;i<5;i++){
+    int count = 1000;
+    int A[count],B[count],C[count];
+    for(int i=0;i<count;i++){
         A[i]=i+1;
-        B[i]=6-i;
+        B[i]=count+1-i;
         C[i]=A[i]*B[i];
     }
     printf("\n");
-    for(int i=0;i<5;i++){
+    for(int i=0;i<count;i++){
         printf("C[%d]=%d ",i,C[i]);
     }
     printf("\n");
